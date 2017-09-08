09413d8ac4d746e75ac0920e2f3e1a7af674a0f6
Natalia <telekinetiscope@gmail.com>
2017-09-08 03:05:19 -0400

    Removed some spaces, changed some quote marks.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.post_list, name='post_list'), # only empty str matches
-    url(r'^post/(?P<pk>\d+)/$',views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
