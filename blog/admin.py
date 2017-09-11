5a34d9529cbd79770d5b820d23e765e5af514644
Natalia <telekinetiscope@gmail.com>
2017-09-11 17:29:12 -0400

    Added comments, user login, and changed to postgres

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import Post
+from .models import Comment
 
 # Register your models here.
 admin.site.register(Post)
-
+admin.site.register(Comment)
--- mysite/settings.py
+++ mysite/settings.py
@@ -75,11 +75,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'djangogirls',
+    'USER': 'nati',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '',
+  }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -120,3 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+# Redirect successful logins to homepage
+
+LOGIN_REDIRECT_URL = '/'
